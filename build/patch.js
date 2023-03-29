cf8f6b5d660f7834b39d60d70a8fedb59b7e00e7
Don Jayamanne <don.jayamanne@outlook.com>
2023-03-29 15:02:58 +1100

    misc

--- build/patch.js
+++ build/patch.js
@@ -1,13 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-const platform = require('os').platform;
-if (platform() === 'win32') {
+const { platform } = require("os");
+if (platform() === "win32") {
   const fs = require("fs");
   const { platform } = require("os");
   const path = require("path");
 
-  const file = path.join(__dirname, 'patch.patch');
+  const file = path.join(__dirname, "patch.patch");
   const contents = fs.readFileSync(file).toString();
-  fs.writeFileSync(file, contents.replace(/\r\n/g, '\n'));
+  fs.writeFileSync(file, contents.replace(/\r\n/g, "\n"));
 }
