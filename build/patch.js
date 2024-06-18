fa43b86cd3fc6873b555d24421ba73f2d2c60b8c
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2024-06-18 09:03:36 -0700

    Fix scoping again

--- build/patch.js
+++ build/patch.js
@@ -1,17 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-function fixWhitespace(file) {
-  const contents = fs.readFileSync(file).toString();
-  fs.writeFileSync(file, contents.replace(/\r\n/g, "\n"));
-}
-
 const { platform } = require("os");
 if (platform() === "win32") {
   const fs = require("fs");
   const { platform } = require("os");
   const path = require("path");
 
-  fixWhitespace(path.join(__dirname, "zeromq.js.patch"));
-  fixWhitespace(path.join(__dirname, "libzmq.patch"));
+  function fixWhitespace(fileName) {
+    const file = path.join(__dirname, fileName);
+    const contents = fs.readFileSync(file).toString();
+    fs.writeFileSync(file, contents.replace(/\r\n/g, "\n"));
+  }
+
+  fixWhitespace("zeromq.js.patch");
+  fixWhitespace("libzmq.patch");
 }
