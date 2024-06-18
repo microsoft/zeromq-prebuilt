d06817a64aaa1bc71c9959595e8e1d80aedadfc0
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2024-06-18 08:50:53 -0700

    Use separate patch file for libzmq

--- build/patch.js
+++ build/patch.js
@@ -1,13 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+function fixWhitespace(fileName) {
+  const file = path.join(__dirname, fileName);
+  const contents = fs.readFileSync(file).toString();
+  fs.writeFileSync(file, contents.replace(/\r\n/g, "\n"));
+}
+
 const { platform } = require("os");
 if (platform() === "win32") {
   const fs = require("fs");
   const { platform } = require("os");
   const path = require("path");
 
-  const file = path.join(__dirname, "patch.patch");
-  const contents = fs.readFileSync(file).toString();
-  fs.writeFileSync(file, contents.replace(/\r\n/g, "\n"));
+  fixWhitespace("zeromq.js.patch");
+  fixWhitespace("libzmq.patch");
 }
